8cc8b8bc64e574ca3fa91f4407e3f4594e0f35ed
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2025-01-04 19:22:02 -0300

    fix(userController): se cambio el update del user de put a patch

--- src/main/java/com/ancore/ancoregaming/user/controllers/UserController.java
+++ src/main/java/com/ancore/ancoregaming/user/controllers/UserController.java
@@ -7,14 +7,7 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ancore.ancoregaming.common.ApiEntityResponse;
 import com.ancore.ancoregaming.common.ApiResponse;
@@ -60,7 +53,7 @@ public class UserController {
     return ApiEntityResponse.of(HttpStatus.OK, response);
   }
 
-  @PutMapping("/{userId}")
+  @PatchMapping("/{userId}")
   public ApiEntityResponse<UserDTO> updateUser(@PathVariable String userId,
       @RequestBody UpdateUserDTO updateUser) {
     User userUpdated = this.userService.updateUser(userId, updateUser);
