770cfc41add623d720ff539b9e76bca2737fbc3a
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2025-05-05 19:19:30 -0300

    feature(DTO): added platform to DTO

--- src/main/java/com/ancore/ancoregaming/checkout/dtos/CheckoutProductDTO.java
+++ src/main/java/com/ancore/ancoregaming/checkout/dtos/CheckoutProductDTO.java
@@ -1,10 +1,12 @@
 package com.ancore.ancoregaming.checkout.dtos;
 
+import com.ancore.ancoregaming.product.model.Platform;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -16,17 +18,19 @@ public class CheckoutProductDTO {
   public BigDecimal discount;
   public String trailer;
   public String mainImage;
+  public List<Platform> platforms;
   
   public CheckoutProductDTO() {
   }
   
-  public CheckoutProductDTO(UUID id, String name, BigDecimal price, BigDecimal discount, String trailer, String mainImage) {
+  public CheckoutProductDTO(UUID id, String name, BigDecimal price, BigDecimal discount, String trailer, String mainImage, List<Platform> platforms) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.discount = discount;
     this.trailer = trailer;
     this.mainImage = mainImage;
+    this.platforms = platforms;
   }
   
   @Override
